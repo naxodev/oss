34171c553b2c6a53b978d6d7d1dcbe7cdb2d6157
Nacho Vazquez <nachovc1410@gmail.com>
2023-09-06 21:08:09 -0300

    chore: move to flat eslint

---
+++ e2e/examples/cloudflare-next-app/eslint.config.js
@@ -0,0 +1,47 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const baseConfig = require('../../../eslint.config.js');
+const globals = require('globals');
+const js = require('@eslint/js');
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+module.exports = [
+  ...baseConfig,
+  ...compat.extends(
+    'plugin:@nx/react-typescript',
+    'next',
+    'next/core-web-vitals'
+  ),
+  { languageOptions: { globals: { ...globals.jest } } },
+  { rules: { '@next/next/no-html-link-for-pages': 'off' } },
+  {
+    files: [
+      'e2e/examples/cloudflare-next-app/**/*.ts',
+      'e2e/examples/cloudflare-next-app/**/*.tsx',
+      'e2e/examples/cloudflare-next-app/**/*.js',
+      'e2e/examples/cloudflare-next-app/**/*.jsx',
+    ],
+    rules: {
+      '@next/next/no-html-link-for-pages': [
+        'error',
+        'e2e/examples/cloudflare-next-app/pages',
+      ],
+    },
+  },
+  {
+    files: [
+      'e2e/examples/cloudflare-next-app/**/*.ts',
+      'e2e/examples/cloudflare-next-app/**/*.tsx',
+    ],
+    rules: {},
+  },
+  {
+    files: [
+      'e2e/examples/cloudflare-next-app/**/*.js',
+      'e2e/examples/cloudflare-next-app/**/*.jsx',
+    ],
+    rules: {},
+  },
+  { ignores: ['e2e/examples/cloudflare-next-app/.next/**/*'] },
+];
