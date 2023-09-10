10bda3e61fe8d37a8f4d3e6f5f64d59687260f6c
Nacho Vazquez <nacho.vazquez.dev@gmail.com>
2023-09-10 13:50:59 -0300

    feat(core): use the latest version of core dependencies (#21)

--- packages/plugins/nx-cloudflare/src/utils/versions.ts
+++ packages/plugins/nx-cloudflare/src/utils/versions.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 export const nxCloudflareVersion = require('../../package.json').version;
 
-export const wranglerVersion = '^3.5.1';
-export const cloudflareWorkersTypeVersions = '^4.20230724.0';
-export const honoVersion = '^2.5.7';
+export const wranglerVersion = '^3.7.0';
+export const cloudflareWorkersTypeVersions = '^4.20230904.0';
+export const honoVersion = '^3.5.8';
