65afc2ad059f4a4218526ac63fdfd544bb4aa82c
Nacho Vazquez <nachovc1410@gmail.com>
2023-10-11 10:08:02 -0300

    feat: add all wrangler dev options to serve executor schema

--- packages/plugins/nx-cloudflare/src/utils/create-cli-options.ts
+++ packages/plugins/nx-cloudflare/src/utils/create-cli-options.ts
@@ -1,11 +1,21 @@
-export function createCliOptions(
-  obj: Record<string, string | number | boolean>
-): string[] {
-  return Object.entries(obj).reduce((arr, [key, value]) => {
-    if (value !== undefined) {
-      const kebabCase = key.replace(/[A-Z]/g, (m) => '-' + m.toLowerCase());
-      arr.push(`--${kebabCase}=${value}`);
+import { names } from '@nx/devkit';
+
+export function createCliOptions<
+  T extends Record<string, string | number | boolean | string[]>
+>(obj: T): string[] {
+  const args: string[] = [];
+
+  for (const key in obj) {
+    const value = obj[key];
+    // kebab-case
+    const arg = names(key).fileName;
+
+    if (Array.isArray(value)) {
+      args.push(`--${arg}=${value.map((v) => v.trim()).join(',')}`);
+    } else {
+      args.push(`--${arg}=${value}`);
     }
-    return arr;
-  }, []);
+  }
+
+  return args;
 }
