99127d0d10115d42dcaf4793a4983eae4f5ec83c
Nacho Vazquez <nachovc1410@gmail.com>
2023-11-18 13:50:40 -0300

    test: add test failing with reported issue

--- e2e/plugins-nx-cloudflare-e2e/tests/next-on-pages.spec.ts
+++ e2e/plugins-nx-cloudflare-e2e/tests/next-on-pages.spec.ts
@@ -1,13 +1,11 @@
 import {
   uniq,
-  tmpProjPath,
   runNxCommand,
   ensureNxProject,
   cleanup,
   readJson,
   updateFile,
 } from '@nx/plugin/testing';
-import { join } from 'path';
 
 describe('Next on Pages', () => {
   beforeEach(() => {
@@ -21,20 +19,17 @@ describe('Next on Pages', () => {
 
   it('should build a NextJS application with the next-on-pages script', async () => {
     const workerapp = uniq('workerapp');
-    const port = 8787;
 
     runNxCommand(
       `generate @nx/next:app --name ${workerapp} --directory="apps" --appDir`
     );
 
-    const projectJson = readJson(
-      join(tmpProjPath(), `apps/${workerapp}/project.json`)
-    );
+    const projectJson = readJson(`apps/${workerapp}/project.json`);
     projectJson.targets.build.executor = '@naxodev/nx-cloudflare:next-build';
-    updateFile(
-      join(tmpProjPath(), `apps/${workerapp}/project.json`),
-      JSON.stringify(projectJson)
-    );
+    console.log('file to update', projectJson);
+    updateFile(`apps/${workerapp}/project.json`, JSON.stringify(projectJson));
+
+    console.log('updated file', readJson(`apps/${workerapp}/project.json`));
 
     const buildResults = runNxCommand(`build ${workerapp}`);
     expect(buildResults).toContain(
