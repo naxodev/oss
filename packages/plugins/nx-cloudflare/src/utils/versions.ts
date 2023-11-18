567b54be87ff730decb235e4a7d6346d5f9b5543
Nacho Vazquez <nachovc1410@gmail.com>
2023-11-18 11:56:02 -0300

    chore: update hono and wrangler

--- packages/plugins/nx-cloudflare/src/utils/versions.ts
+++ packages/plugins/nx-cloudflare/src/utils/versions.ts
@@ -1,6 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const nxCloudflareVersion = require('../../package.json').version;
 
-export const wranglerVersion = '^3.15.0';
+export const wranglerVersion = '^3.16.0';
 export const cloudflareWorkersTypeVersions = '^4.20231025.0';
-export const honoVersion = '^3.9.2';
+export const honoVersion = '^3.10.1';
