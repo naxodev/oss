0a9da0d3d17792ed31335cb98ef807e33e1a1f1b
Nacho Vazquez <nachovc1410@gmail.com>
2023-11-18 22:23:31 -0300

    fix: revert the implementation to use npx

--- e2e/plugins-nx-cloudflare-e2e/tests/next-on-pages.spec.ts
+++ e2e/plugins-nx-cloudflare-e2e/tests/next-on-pages.spec.ts
@@ -7,29 +7,26 @@ import {
   updateFile,
 } from '@nx/plugin/testing';
 
+const workerapp = uniq('workerapp');
+
 describe('Next on Pages', () => {
   beforeEach(() => {
     ensureNxProject(
       '@naxodev/nx-cloudflare',
       'dist/packages/plugins/nx-cloudflare'
     );
+
+    runNxCommand(
+      `generate @nx/next:app --name ${workerapp} --directory="apps" --appDir`
+    );
   });
 
   afterEach(() => cleanup());
 
   it('should build a NextJS application with the next-on-pages script', async () => {
-    const workerapp = uniq('workerapp');
-
-    runNxCommand(
-      `generate @nx/next:app --name ${workerapp} --directory="apps" --appDir`
-    );
-
     const projectJson = readJson(`apps/${workerapp}/project.json`);
     projectJson.targets.build.executor = '@naxodev/nx-cloudflare:next-build';
-    console.log('file to update', projectJson);
     updateFile(`apps/${workerapp}/project.json`, JSON.stringify(projectJson));
-
-    console.log('updated file', readJson(`apps/${workerapp}/project.json`));
 
     const buildResults = runNxCommand(`build ${workerapp}`);
     expect(buildResults).toContain(
--- packages/plugins/nx-cloudflare/src/executors/next-build/build.impl.ts
+++ packages/plugins/nx-cloudflare/src/executors/next-build/build.impl.ts
@@ -3,14 +3,20 @@ import 'dotenv/config';
 import {
   detectPackageManager,
   ExecutorContext,
+  getPackageManagerCommand,
   logger,
   readJsonFile,
-  workspaceRoot,
   writeJsonFile,
 } from '@nx/devkit';
 import { createLockFile, createPackageJson, getLockFileName } from '@nx/js';
-import { join, resolve as pathResolve } from 'path';
-import { copySync, existsSync, mkdir, writeFileSync } from 'fs-extra';
+import { join } from 'path';
+import {
+  copySync,
+  ensureDirSync,
+  existsSync,
+  mkdir,
+  writeFileSync,
+} from 'fs-extra';
 import { gte } from 'semver';
 import { directoryExists } from '@nx/workspace/src/utilities/fileutils';
 import { checkAndCleanWithSemver } from '@nx/devkit/src/utils/semver';
@@ -19,15 +25,15 @@ import { updatePackageJson } from './lib/update-package-json';
 import { createNextConfigFile } from './lib/create-next-config-file';
 import { checkPublicDirectory } from './lib/check-project';
 import { NextBuildBuilderOptions } from '../../utils/types';
-import { ChildProcess, fork } from 'child_process';
+import { execSync, ExecSyncOptions } from 'child_process';
 import { createCliOptions } from '../../utils/create-cli-options';
-
-let childProcess: ChildProcess;
 
 // This executor is a modified version of the original `@nrwl/next:build` executor.
 // It's main modification is to use the cloudflare next-on-pages package.
 // Because the Cloudflare builder doesn't allow us to locate the build output outside the project root directory
 // we need to change the output path config options to the project root directory and then copy the build output to the desired output path.
+// We also move from invoking the bin script to executing the command with the `execSync` function. This is because the bin script
+// is not exported on the package.json
 export default async function buildExecutor(
   options: NextBuildBuilderOptions,
   context: ExecutorContext
@@ -54,21 +60,18 @@ export default async function buildExecutor(
   if (hasReact18) {
     process.env['__NEXT_REACT_ROOT'] ||= 'true';
   }
+  ensureDirSync(join(projectRoot, '.vercel'));
 
   const { outputPath: originalOutputPath } = options;
   // Set the outputPath to the projectRoot to bypass cloudflare builded limitations.
   options.outputPath = projectRoot;
 
   try {
-    await runCliBuild(workspaceRoot, projectRoot, options);
+    runCliBuild(projectRoot, options);
   } catch (error) {
     logger.error(`Error occurred while trying to run the build command`);
     logger.error(error);
     return { success: false };
-  } finally {
-    if (childProcess) {
-      childProcess.kill();
-    }
   }
 
   if (!directoryExists(options.outputPath)) {
@@ -130,11 +133,7 @@ export default async function buildExecutor(
   return { success: true };
 }
 
-function runCliBuild(
-  workspaceRoot: string,
-  projectRoot: string,
-  options: NextBuildBuilderOptions
-) {
+function runCliBuild(projectRoot: string, options: NextBuildBuilderOptions) {
   const { experimentalAppOnly, profile, debug, outputPath } = options;
 
   // Set output path here since it can also be set via CLI
@@ -142,31 +141,16 @@ function runCliBuild(
   process.env.NX_NEXT_OUTPUT_PATH ??= outputPath;
 
   const args = createCliOptions({ experimentalAppOnly, profile, debug });
-  return new Promise((resolve, reject) => {
-    childProcess = fork(
-      require.resolve('@cloudflare/next-on-pages/bin'),
-      [...args],
-      {
-        cwd: pathResolve(workspaceRoot, projectRoot),
-        stdio: 'inherit',
-        env: process.env,
-      }
-    );
 
-    // Ensure the child process is killed when the parent exits
-    process.on('exit', () => childProcess.kill());
-    process.on('SIGTERM', () => childProcess.kill());
+  const pmc = getPackageManagerCommand('npm');
+  const execSyncOptions: ExecSyncOptions = {
+    stdio: ['inherit', 'inherit', 'inherit'],
+    encoding: 'utf-8',
+    cwd: projectRoot,
+  };
 
-    childProcess.on('error', (err) => {
-      reject(err);
-    });
-
-    childProcess.on('exit', (code) => {
-      if (code === 0) {
-        resolve(code);
-      } else {
-        reject(code);
-      }
-    });
-  });
+  execSync(
+    `${pmc.exec} @cloudflare/next-on-pages ${args.join(' ')}`,
+    execSyncOptions
+  );
 }
