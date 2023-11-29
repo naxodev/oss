4e32f5304e73f3c5839458b20f2fd3f04b20be22
Nacho Vazquez <nachovc1410@gmail.com>
2023-11-29 10:10:23 -0300

    chore: update packages

--- packages/plugins/nx-cloudflare/src/utils/versions.ts
+++ packages/plugins/nx-cloudflare/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxCloudflareVersion = require('../../package.json').version;
 
-export const wranglerVersion = '^3.16.0';
-export const cloudflareWorkersTypeVersions = '^4.20231025.0';
-export const honoVersion = '^3.10.1';
+export const wranglerVersion = '^3.17.1';
+export const cloudflareWorkersTypeVersions = '^4.20231121.0';
+export const honoVersion = '^3.10.3';
