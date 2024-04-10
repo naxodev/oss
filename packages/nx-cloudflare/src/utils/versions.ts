27253555e7268dce4301383833aef5196eed1554
Nacho Vazquez <nachovc1410@gmail.com>
2024-04-10 09:12:34 -0300

    chore: update other core versions

--- packages/nx-cloudflare/src/utils/versions.ts
+++ packages/nx-cloudflare/src/utils/versions.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 export const nxCloudflareVersion = require('../../package.json').version;
 
-export const wranglerVersion = '^3.28.1';
-export const cloudflareWorkersTypeVersions = '^4.20240208.0';
-export const honoVersion = '^4.0.1';
+export const wranglerVersion = '^3.48.0';
+export const cloudflareWorkersTypeVersions = '^4.20240405.0';
+export const honoVersion = '^4.2.3';
