b541f385d0bdb63e118993fb6bb735e328cf1e9a
Nacho Vazquez <nacho.vazquez.dev@gmail.com>
2024-06-29 19:46:09 -0300

    chore: update to nx 19.3 (#51)
    * chore: update to nx 19.3
    
    * fix: pnpm lock
    
    * chore: migrate to pnpm 9
    
    * fix: migration to pnpm 9

--- packages/nx-cloudflare/src/utils/versions.ts
+++ packages/nx-cloudflare/src/utils/versions.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 export const nxCloudflareVersion = require('../../package.json').version;
 
-export const wranglerVersion = '^3.57.0';
+export const wranglerVersion = '^3.62.0';
 export const cloudflareWorkersTypeVersions = '^4.20240512.0';
-export const honoVersion = '^4.3.7';
+export const honoVersion = '^4.4.9';
