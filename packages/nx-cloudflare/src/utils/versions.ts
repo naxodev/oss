a8fe31f38493e7248fe25be23806da8f22577cf6
Nik <divjak.nik@gmail.com>
2025-04-08 15:24:34 +0200

    chore(nx-cloudflare): upgrade wrangler from 3.109.1 to 4.7.1 (#63)
    * chore(nx-cloudflare): upgrade wrangler from 3.109.1 to 4.7.1
    
    * Update versions.ts

--- packages/nx-cloudflare/src/utils/versions.ts
+++ packages/nx-cloudflare/src/utils/versions.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 export const nxCloudflareVersion = require('../../package.json').version;
 
-export const wranglerVersion = '^3.109.1';
+export const wranglerVersion = '^4.7.1';
 export const cloudflareWorkersTypeVersions = '^4.20250214.0';
 export const honoVersion = '^4.7.1';
 export const vitestPoolWorkersVersion = '^0.6.16';
