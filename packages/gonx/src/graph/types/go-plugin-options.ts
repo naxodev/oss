4d7f48b697792b10fcc49e8f266b27ac5309538e
Konrad <konrad.koch@assemblio.de>
2025-05-19 13:53:42 +0200

    feat(gonx): relocate `skipGoDependencyCheck` to updated plugin options
    Moved `skipGoDependencyCheck` from `type.d.ts` to `go-plugin-options.ts` for better structure and maintainability.
    
    No functional changes were made.

--- packages/gonx/src/graph/types/go-plugin-options.ts
+++ packages/gonx/src/graph/types/go-plugin-options.ts
@@ -7,4 +7,12 @@ export interface GoPluginOptions {
   lintTargetName?: string;
   releasePublishTargetName?: string;
   tagName?: string;
+  /**
+   * If true, the plugin will not require
+   * to have Go installed to compute a Nx workspace graph.
+   *
+   * Be aware that if Go is not installed, the plugin will not be able
+   * to detect dependencies between Go projects and this is source of misunderstanding.
+   */
+  skipGoDependencyCheck?: boolean;
 }
