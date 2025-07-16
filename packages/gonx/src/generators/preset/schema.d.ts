39907ad12104a2f4b94b551014ad3c287e595054
Nacho Vazquez <nacho@naxo.dev>
2025-07-15 23:57:54 -0300

    feat: add go-blueprint to presets

--- packages/gonx/src/generators/preset/schema.d.ts
+++ packages/gonx/src/generators/preset/schema.d.ts
@@ -2,6 +2,28 @@ import { GeneratorSchema } from '../../utils';
 
 export interface PresetGeneratorSchema extends GeneratorSchema {
   skipFormat?: boolean;
-  type?: 'binary' | 'library';
+  type?: 'binary' | 'library' | 'go-blueprint';
   directory: string;
+  name?: string;
+  tags?: string;
+  addGoDotWork?: boolean;
+  // Go Blueprint specific options
+  framework?:
+    | 'chi'
+    | 'gin'
+    | 'fiber'
+    | 'gorilla/mux'
+    | 'httprouter'
+    | 'standard-library'
+    | 'echo';
+  driver?:
+    | 'mysql'
+    | 'postgres'
+    | 'sqlite'
+    | 'mongo'
+    | 'redis'
+    | 'scylla'
+    | 'none';
+  git?: 'commit' | 'stage' | 'skip';
+  feature?: string[];
 }
