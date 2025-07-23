6c9c7556198aa0b76ad1b60a1352ee182b7d58d0
abelpenton <2235penton@gmail.com>
2025-07-23 16:31:07 -0300

    feat(docs): add astro config

---
+++ docs/.astro/integrations/_astrojs_starlight/i18n-plugins.d.ts
@@ -0,0 +1,7 @@
+declare namespace StarlightApp {
+	type PluginUIStringKeys = {
+		'starlight-site-graph.graph': string;
+		'starlight-site-graph.backlinks': string;
+	};
+	interface I18n extends PluginUIStringKeys {}
+}
