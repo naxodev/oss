2026d155b4190c4f4419be777e8763a1e060beea
abelpenton <2235penton@gmail.com>
2025-07-23 10:28:22 -0300

    fix(start-registry): restore start registry configuration

--- tools/scripts/start-local-registry.ts
+++ tools/scripts/start-local-registry.ts
@@ -3,7 +3,7 @@
  * It is meant to be called in jest's globalSetup.
  */
 import { startLocalRegistry } from '@nx/js/plugins/jest/local-registry';
-import { releasePublish, releaseVersion } from 'nx/release';
+import { execFileSync } from 'child_process';
 
 export default async () => {
   // local registry target to run
@@ -17,18 +17,10 @@ export default async () => {
     verbose: false,
   });
 
-  await releaseVersion({
-    specifier: '0.0.0-e2e',
-    stageChanges: false,
-    gitCommit: false,
-    gitTag: false,
-    firstRelease: true,
-    versionActionsOptionsOverrides: {
-      skipLockFileUpdate: true,
-    },
-  });
-  await releasePublish({
-    tag: 'e2e',
-    firstRelease: true,
-  });
+  const nx = require.resolve('nx');
+  execFileSync(
+    nx,
+    ['run-many', '--targets', 'publish', '--ver', '0.0.0-e2e', '--tag', 'e2e'],
+    { env: process.env, stdio: 'inherit' }
+  );
 };
