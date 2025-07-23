64acf6c51012d149e5cfcf01cc34a76e090ca506
abelpenton <2235penton@gmail.com>
2025-07-23 16:39:13 -0300

    fix(docs): .astro folder

--- docs/.astro/integrations/_astrojs_starlight/i18n-plugins.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace StarlightApp {
-	type PluginUIStringKeys = {
-		'starlight-site-graph.graph': string;
-		'starlight-site-graph.backlinks': string;
-	};
-	interface I18n extends PluginUIStringKeys {}
-}
