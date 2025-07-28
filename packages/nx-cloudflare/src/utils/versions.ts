7354bf4f4dbcd89cfbcb0dee0e9adae351067992
Nacho Vazquez <nacho@naxo.dev>
2025-07-27 21:14:27 -0300

    chore: update dependencies

--- packages/nx-cloudflare/src/utils/versions.ts
+++ packages/nx-cloudflare/src/utils/versions.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 export const nxCloudflareVersion = require('../../package.json').version;
 
-export const wranglerVersion = '^4.7.1';
-export const cloudflareWorkersTypeVersions = '^4.20250214.0';
-export const honoVersion = '^4.7.1';
+export const wranglerVersion = '^4.26.0';
+export const cloudflareWorkersTypeVersions = '^4.20250404.0';
+export const honoVersion = '^4.8.9';
 export const vitestPoolWorkersVersion = '^0.6.16';
-export const vitestVersion = '2.1.8';
+export const vitestVersion = '^3.2.4';
